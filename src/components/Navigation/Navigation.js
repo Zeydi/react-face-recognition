8c4cc65a8d175dddeff4fd463a1c6141a5d504bd
Zeydi <oussema.zeidi@ieee.org>
2019-02-12 01:41:44 +0100

    added navigation component

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const styles = {
+  display: "flex",
+  justifyContent: "flex-end"
+};
+const Navigation = () => {
+  return (
+    <nav style={styles}>
+      <p className="f3 link dim black underline pa3 pointer">Sign Out</p>
+    </nav>
+  );
+};
+
+export default Navigation;
