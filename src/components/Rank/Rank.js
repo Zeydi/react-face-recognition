622fcd5693435bc71122b191d3dc753f3a2cdf42
Zeydi <oussema.zeidi@ieee.org>
2019-02-13 16:13:33 +0100

    added Rank component

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Rank = () => {
+  return (
+    <div>
+      <div className="Black f3">{"Oussama, your current rank is ..."}</div>
+      <div className="Black f1">{"#5"}</div>
+    </div>
+  );
+};
+
+export default Rank;
